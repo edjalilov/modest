6358ea4dacefd803476107f713d926248a32580d
Elyor Djalilov <edjalilov.dev@gmail.com>
2020-06-10 14:41:43 +0500

    add scroll toTop

--- js/scroll.js
+++ js/scroll.js
@@ -12,3 +12,24 @@ for (let anchor of anchors) {
         })
     })
 }
+
+let btn = document.querySelector('.scroll-btn');
+let timer;
+
+btn.addEventListener('click', toTop);
+
+function toTop() {
+    let scrolled = window.pageYOffset;
+    if (scrolled > 0) {
+        scrollTo(0, scrolled -= 100);
+        timer = setTimeout(toTop, 10);
+    }
+}
+
+window.addEventListener('scroll', function () {
+    if (pageYOffset > 800) {
+        btn.style.display = 'block';
+    } else {
+        btn.style.display = 'none';
+    }
+})
