12225ca17412031c33cf9b98f539d520d9e06d4c
Elyor Djalilov <edjalilov.dev@gmail.com>
2020-06-25 22:03:56 +0500

    add event scroll

---
+++ js/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    $(window).scrollTop(0);
+    $(window).scroll(function () {
+        let scroll = $(this).scrollTop();
+        if (scroll > 500) {
+            $('.scroll-btn').fadeIn();
+        } else {
+            $('.scroll-btn').fadeOut();
+        }
+    })
+
+    $('.scroll-btn').click(function () {
+        $('html').animate({
+            scrollTop: 0
+        }, 1000)
+    })
+
+    let links = $('.menu__link');
+    links.click(function (e) {
+        e.preventDefault();
+        links.removeClass('active');
+        let menuHeight = $('.menu__link').innerHeight();
+        let id = $(this).addClass('active').attr('href');
+        let target = $(id).offset().top - menuHeight;
+
+
+        $('html').animate({
+            scrollTop: target
+        }, 1000)
+    })
+
+    $(window).scroll(function () {
+        let scroll = $(this).scrollTop();
+        links.each(function () {
+            let attr = $(this).attr('href');
+            let position = $(attr).offset().top - 130;
+            if (scroll >= position) {
+                links.removeClass('active');
+                $(this).addClass('active');
+            }
+        })
+    })
+
+
+
+
+
+
+});
